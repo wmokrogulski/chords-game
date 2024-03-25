7d2f9ad6f6ee118346d7f220ba6482d0a6d0cdb4
Wawrzyniec Mokrogulski <mokrogulski1b@gmail.com>
2024-03-25 21:53:01 +0100

    updated as sub website

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import musicpy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-answer=None
-chord=None
+answer = None
+chord = None
 
-names_dict={'D7no1':'D7-1','D9b':'D9>','D9no1':'D9-1','D9bno1':'D9>-1','D6':'D76-5','D6b':'D76>-5'}
-rev_names_dict={v:k for k,v in names_dict.items()}
+names_dict = {'D7no1': 'D7-1', 'D9b': 'D9>', 'D9no1': 'D9-1',
+              'D9bno1': 'D9>-1', 'D6': 'D76-5', 'D6b': 'D76>-5'}
+rev_names_dict = {v: k for k, v in names_dict.items()}
 
 
-@app.route("/")
+@app.route("/dominants/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("dominants.html")
 
 
 @socketio.on('play')
@@ -23,33 +24,33 @@ def handle_play():
     global answer
     global chord
     if answer is None:
-      chord,answer=random_dominant()
+        chord, answer = random_dominant()
     musicpy.play(chord, wait=True, bpm=60)
+
 
 @socketio.on('next')
 def handle_play():
     global answer
-    answer=None
+    answer = None
 
 
 @socketio.on('answer')
 def handle_answer(data):
     global names_dict
     global rev_names_dict
-    chord_type=names_dict.get(data['chordType'],data['chordType'])
+    chord_type = names_dict.get(data['chordType'], data['chordType'])
 
+    user_answer = f'{chord_type}/{data["chordRoot"]}'
+    chord_type, root = answer.split('/')
+    chord_type = rev_names_dict.get(chord_type, chord_type)
+    root = f'root{root}'
 
-    user_answer=f'{chord_type}/{data["chordRoot"]}'
-    chord_type,root=answer.split('/')
-    chord_type=rev_names_dict.get(chord_type,chord_type)
-    root=f'root{root}'
-    
     print(answer, user_answer)
 
-    correct=answer==user_answer
+    correct = answer == user_answer
 
-
-    socketio.emit('answer_returned',{'correct':correct,'chord_type':chord_type,'chord_root':root})
+    socketio.emit('answer_returned', {
+                  'correct': correct, 'chord_type': chord_type, 'chord_root': root})
 
 
 if __name__ == '__main__':
