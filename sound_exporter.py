1cc9d15246422d51c1cf6cf96434b9267f08eaa1
Wawrzyniec Mokrogulski <mokrogulski1b@gmail.com>
2024-03-26 15:59:50 +0100

    changed to mp3

--- sound_exporter.py
+++ sound_exporter.py
@@ -5,16 +5,16 @@ from random import choice
 
 
 def export_sound(sound):
-  filename = 'test.wav'
+  filename = 'test.mp3'
 
   new_song = daw(1, name='sound')
   new_song.load(0, 'MuseScore_General.sf3')
 
-  new_song.export(sound, channel_num=0, bpm=40, mode='wav',
+  new_song.export(sound, channel_num=0, bpm=40, mode='mp3',
                   filename=filename)
 
   with open(filename, 'rb') as f:
       text = base64.b64encode(f.read())
   b64 = text.decode()
-  audio_b64 = f'data:audio/wav;base64,{b64}'
+  audio_b64 = f'data:audio/mp3;base64,{b64}'
   return audio_b64
